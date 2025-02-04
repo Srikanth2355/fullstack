574c2774503351c90247bd17b6d10361b903e8ea
Srikanth <gubbasrikanth2355@gmail.com>
2025-02-04 07:23:44 +0530

    Implemented Logout Functionality along with global loader

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,20 +3,21 @@ import Login from './Login'
 import Home from './Home'
 import Register from './Register'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { LoadingProvider } from './utils/loader.jsx';
 
 function App() {
 
   return (
-    <>
+    <LoadingProvider>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     
     </BrowserRouter>
-    </>
+    </LoadingProvider>
   )
 }
 
--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { Children } from 'react';
+import { Layout, Menu, Avatar, Dropdown, Space, notification } from 'antd';
+import { UserOutlined, LogoutOutlined, DownOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+import { useLoading } from './utils/loader.jsx';
+import axiosInstance from './utils/axios';
+
+const { Header } = Layout;
 
 function Home() {
+  const navigate = useNavigate();
+  const {showLoading, hideLoading} = useLoading();
+
+  const handleLogout = () => {
+    showLoading();
+      axiosInstance.get('/user/logout')
+      .then((response) => {
+        if(response.status === 200){
+          navigate('/login');
+          notification.success({
+            message: 'Success',
+            description: "Logout Successfull",
+            duration: 3
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+      .finally(() => {
+        hideLoading();
+      }); 
+
+  }
   return (
-    <div>Home</div>
+    <>
+      <Header className="bg-white p-0"> 
+        <div className="flex items-center px-6"> 
+          <div className="flex-1"> 
+            <p className='text-2xl font-semibold'>Logo</p>
+          </div>
+          <div className="flex items-center space-x-4"> 
+            <Dropdown menu={{ items: [{ key: '1', label: <p className='flex justify-between' onClick={handleLogout}><span>Logout</span> <LogoutOutlined /></p> }] }}>
+              <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                  <Avatar size="medium" icon={<UserOutlined />} />
+                  Srikanth <DownOutlined />
+                </Space>
+              </a>
+            </Dropdown>
+          </div>
+        </div>
+      </Header>
+    </>
   )
 }
 
