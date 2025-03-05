cb2ba330732d79b976b104a407d33b1c105139ab
Srikanth <gubbasrikanth2355@gmail.com>
2025-03-05 08:10:06 +0530

    uncommented server code

--- Api/server.js
+++ Api/server.js
@@ -75,12 +75,12 @@ app.get("/api/healthcheck/checkdatabaseservices", (req, res) => {
 })
 
 // Serve the static files from the React build folder
-// app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 // // Catch-all route to serve index.html for React Router
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
-//   });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
+  });
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server running on port ${port}`))   
